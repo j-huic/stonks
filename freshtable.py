d3ac25110e4425a9d17512bddc286a26556c284b
Janko <janko.huic@gmail.com>
2024-07-29 10:59:50 +0200

    script to download fresh data, asks for max workers, name of db/table

---
+++ freshtable.py
@@ -0,0 +1,16 @@
+import sqlite3
+import pandas as pd
+from functions import *
+
+datelist = get_trading_days()
+max_workers = input('Enter max workers: ')
+df = datelist_to_df_parallel(datelist, max_workers=max_workers)
+
+database = input('Enter database name: ')
+table = input('Enter table name: ')
+
+conn = sqlite3.connect(database)
+df.columns = get_table_colnames(table)
+df.to_sql(table, conn, if_exists='append', index=False)
+conn.close()
+
